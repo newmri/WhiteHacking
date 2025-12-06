836bfb31b82c57cf134262d28e27b123fdf69c6d
newmri <newmri@naver.com>
2025-12-06 16:13:05 +0900

    mac_changer 개선

--- Python/mac_changer.py
+++ Python/mac_changer.py
@@ -2,6 +2,7 @@
 import subprocess
 import os
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -22,8 +23,27 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] MAC address not found")
+
 os.system('chcp 65001')
 
 options = get_arguments()
 
+current_mac = get_current_mac(options.interface)
+print("[+] Current MAC address: " + str(current_mac))
+
 change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address changed successfully")
+else:
+    print("[-] MAC address not changed successfully")
+
